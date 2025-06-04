8ccf5801a432d667b59487207b54a141d664998f
Stetson Wendel <stetsonwendel@gmail.com>
2025-06-04 14:47:54 -0600

    Initial commit - Unreal Engine 4 project structure

---
+++ Source/SparkOfDawnRPG/SparkOfDawnRPGGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SparkOfDawnRPGGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASparkOfDawnRPGGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASparkOfDawnRPGGameMode();
+};
+
+
+
